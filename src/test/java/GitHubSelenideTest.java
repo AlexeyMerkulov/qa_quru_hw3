7110bbaac340aac35154fab3f8b01d623c93c457
Merkulov Aleksey <AMerkulov@bellintegrator.com>
2023-06-18 15:13:32 +0300

    Исправление замечаний #1

--- src/test/java/GitHubSelenideTest.java
+++ src/test/java/GitHubSelenideTest.java
@@ -3,25 +3,23 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class GitHubSelenideTest {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://github.com/selenide";
+        Configuration.baseUrl = "https://github.com";
     }
 
     @Test
     void testJunit5CodeExampleIsDisplayed() {
-        open("/selenide");
+        open("/selenide/selenide");
         $("#wiki-tab").click();
-        $("li.Box-row.wiki-more-pages-link button").click();
-        $("li.Box-row a[href='/selenide/selenide/wiki/SoftAssertions']")
-                .shouldBe(visible)
-                .shouldHave(text("SoftAssertions"))
-                .click();
+        $(".wiki-more-pages-link button").click();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
         $("#wiki-content").shouldHave(text(
                 "@ExtendWith({SoftAssertsExtension.class})\n" +
                         "class Tests {\n" +
